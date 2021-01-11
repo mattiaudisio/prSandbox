64793fbf2ade28ed9d406042c84f082761011908
Mattia Audisio <mattia.audisio.01@gmail.com>
2021-01-11 19:59:03 +0100

    update

---
+++ Construct 2/VideogiocoPlatform/VideogiocoPlatform/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1603739505,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/mappa.png",
+		"images/player-sheet0.png",
+		"images/collisionbox-sheet0.png",
+		"images/mappa3.png",
+		"images/camera-sheet0.png",
+		"images/nuvola-sheet0.png",
+		"images/overlap-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}

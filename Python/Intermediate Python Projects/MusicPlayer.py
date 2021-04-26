abd15b859dce7dd328e07460f6539dd8d7066d22
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2021-04-26 12:25:20 +0200

    Update MusicPlayer.py

--- Python/Intermediate Python Projects/MusicPlayer.py
+++ Python/Intermediate Python Projects/MusicPlayer.py
@@ -1,26 +1,58 @@
-# Import the required module for text
-# to speech conversion
-from gtts import gTTS
-
-# This module is imported so that we can
-# play the converted audio
+import pygame
+from pygame import mixer
+from tkinter import *
 import os
 
-# The text that you want to convert to audio
-mytext = 'Welcome to geeksforgeeks!'
+def playsong():
+    currentsong=playlist.get(ACTIVE)
+    print(currentsong)
+    mixer.music.load(currentsong)
+    songstatus.set("Playing")
+    mixer.music.play()
 
-# Language in which you want to convert
-language = 'en'
+def pausesong():
+    songstatus.set("Paused")
+    mixer.music.pause()
 
-# Passing the text and language to the engine,
-# here we have marked slow=False. Which tells
-# the module that the converted audio should
-# have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
+def stopsong():
+    songstatus.set("Stopped")
+    mixer.music.stop()
 
-# Saving the converted audio in a mp3 file named
-# welcome
-myobj.save("welcome.mp3")
+def resumesong():
+    songstatus.set("Resuming")
+    mixer.music.unpause()    
 
-# Playing the converted file
-os.system("mpg321 welcome.mp3")
+root=Tk()
+root.title('ProjectGurukul Music player project')
+
+mixer.init()
+songstatus=StringVar()
+songstatus.set("choosing")
+
+#playlist---------------
+
+playlist=Listbox(root,selectmode=SINGLE,bg="DodgerBlue2",fg="white",font=('arial',15),width=40)
+playlist.grid(columnspan=5)
+
+os.chdir(r'C:\Users\BOSS\Desktop\MyPlaylist')
+songs=os.listdir()
+for s in songs:
+    playlist.insert(END,s)
+
+playbtn=Button(root,text="play",command=playsong)
+playbtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+playbtn.grid(row=1,column=0)
+
+pausebtn=Button(root,text="Pause",command=pausesong)
+pausebtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+pausebtn.grid(row=1,column=1)
+
+stopbtn=Button(root,text="Stop",command=stopsong)
+stopbtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+stopbtn.grid(row=1,column=2)
+
+Resumebtn=Button(root,text="Resume",command=resumesong)
+Resumebtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+Resumebtn.grid(row=1,column=3)
+
+mainloop()
